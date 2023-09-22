28b1cba2d6be87006eb3c3d42b227af388e9a441
Kashyrets Ivanna <ivanna.kashyrets@ex2.team>
2023-09-22 13:21:54 +0300

    fix: redeploy

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -3,6 +3,7 @@ import styles from './Button.module.scss'
 
 function BigButton({
     text, clickButton }: { text: string, clickButton: () => void }) {
+        
     return (
         <button onClick={clickButton} className={styles.button}>{text}</button>
     )
