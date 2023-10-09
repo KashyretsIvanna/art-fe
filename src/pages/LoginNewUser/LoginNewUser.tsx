821e2ffd1576e251ff5b2d25992f764311879665
IvannaKashyrets <128041870+IvannaKashyrets@users.noreply.github.com>
2023-10-09 08:58:23 +0300

    fix: error message (#30)

--- src/pages/LoginNewUser/LoginNewUser.tsx
+++ src/pages/LoginNewUser/LoginNewUser.tsx
@@ -18,6 +18,11 @@ function LoginNewUser() {
     const [password, setPassword] = useState('')
     const [name, setName] = useState('')
 
+    const [isPasswordError, setIsPasswordError] = useState<boolean>(false)
+    const [isEmailError, setIsEmailError] = useState<boolean>(false)
+    const [isNameError, setIsNameError] = useState<boolean>(false)
+
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -25,47 +30,20 @@ function LoginNewUser() {
         useRegisterNewUserMutation();
 
     const clickButton = async () => {
-        if (password && name && email && !error) {
-            await registerNewUser({ email, password, name });
-        }
+        await registerNewUser({ email, password, name });
 
     }
     useEffect(() => {
+
+
         if (registrationError) {
             setError(registrationError.data.message)
+            setIsEmailError(true)
+            setIsNameError(true)
+            setIsPasswordError(true)
+
         }
     }, [registrationError])
-
-    const onChangeInputs = () => {
-        const lowerCaseLetters = /[a-z]/g;
-        const upperCaseLetters = /[A-Z]/g;
-        const numbers = /[0-9]/g;
-        const oneSpecCharacter = /[?=.*]/g
-        const emailRegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g
-
-
-        if (password && !password.match(lowerCaseLetters)) {
-            setError('Password should contain lowercase letter')
-        } else if (password && !password.match(upperCaseLetters)) {
-            setError('Password should contain uppercase letter')
-        } else if (password && !password.match(numbers)) {
-            setError('Password should contain numbers')
-        } else if (password && password.length < 8) {
-            setError('Minimum password length ')
-        } else if (password && !password.match(oneSpecCharacter)) {
-            setError('Minimum one special character')
-        } else if (email && !email.match(emailRegExp)) {
-            setError('Bad email')
-        } else {
-            setError('')
-        }
-    }
-
-
-    useEffect(() => {
-        onChangeInputs()
-    }, [password, email])
-
 
 
     useEffect(() => {
@@ -81,9 +59,9 @@ function LoginNewUser() {
         <AdminLayout headerRight={
             null} navigationItems={['All Clients', 'Login']} pageHeader='Login' >
             <div className={styles.inputs_container}>
-                <InputWithImage maxLength={30} type='text' imgHEight='16px' imgWidth='16px' isError={error ? true : false} data={name} setData={setName} placeholder='Art Date' img={PersonImg} marginTop={'22px'} marginLeft={'11px'} />
-                <InputWithImage type='text' imgHEight='10px' imgWidth='12px' isError={error ? true : false} data={email} setData={setEmail} placeholder='Art@dating.com' img={emailIcon} marginTop={'26px'} marginLeft={'13px'} />
-                <InputWithImage type='password' isError={error ? true : false} imgHEight='16px' imgWidth='16px' data={password} setData={setPassword} placeholder='Password' img={passwordIcon} marginTop={'22px'} marginLeft={'11px'} />
+                <InputWithImage maxLength={30} type='text' imgHEight='16px' imgWidth='16px' isError={isNameError} data={name} setData={setName} placeholder='Art Date' img={PersonImg} marginTop={'22px'} marginLeft={'11px'} />
+                <InputWithImage type='text' imgHEight='10px' imgWidth='12px' isError={isEmailError} data={email} setData={setEmail} placeholder='Art@dating.com' img={emailIcon} marginTop={'26px'} marginLeft={'13px'} />
+                <InputWithImage type='password' isError={isPasswordError} imgHEight='16px' imgWidth='16px' data={password} setData={setPassword} placeholder='Password' img={passwordIcon} marginTop={'22px'} marginLeft={'11px'} />
                 <p className={styles.login__error_massage}>{error}</p>
             </div>
 
